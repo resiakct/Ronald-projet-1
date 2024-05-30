6c7db0aa9c201573f17232308cb01bc605ea0b9c
SoryRonald <165383634+SoryRonald@users.noreply.github.com>
2024-05-30 13:21:22 +0000

    Create Upt.js

---
+++ scripts/cmds/Upt.js
@@ -0,0 +1,28 @@
+module.exports = {
+  config: {
+    name: "upt",
+    aliases: ["upt", "up"],
+    version: "1.0",
+    author: "XyryllPanget",
+    role: 0,
+    shortDescription: {
+      en: "Displays the uptime of the bot."
+    },
+    longDescription: {
+      en: "Displays the amount of time that the bot has been running for."
+    },
+    category: "System",
+    guide: {
+      en: "Use {p}uptime to display the uptime of the bot."
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+    const uptime = process.uptime();
+    const seconds = Math.floor(uptime % 60);
+    const minutes = Math.floor((uptime / 60) % 60);
+    const hours = Math.floor((uptime / (60 * 60)) % 24);
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const uptimeString = `\n🎯  ${hours} heures\n  \n🎯  ${minutes} minutes\n  \n🎯  ${seconds} secondes\n`;
+    api.sendMessage(`\n╭═══════upt═══════╮\n│⏰➮ ${uptimeString} │ \n╰════════════════╯\n`, event.threadID);
+  }
+};
