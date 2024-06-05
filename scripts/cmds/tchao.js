8bdb20eef297699c334be86018ac9dd9c548e9ea
Sory Ronald <165383634+SoryRonald@users.noreply.github.com>
2024-06-05 21:41:52 +0000

    Create tchao.js

---
+++ scripts/cmds/tchao.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const request = require("request");
+module.exports = {
+	config: {
+		name: "tchao",
+		aliases: [],
+		version: "1.0",
+		author: "Shïsûį Dånïęl",
+		countDown: 5,
+		role: 2,
+		shortDescription: "bot will leave gc",
+		longDescription: "",
+		category: "admin",
+		guide: {
+			vi: "{pn} [tid,blank]",
+			en: "{pn} [tid,blank]"
+		}
+	},
+
+	onStart: async function ({ api,event,args, message }) {
+ var id;
+ if (!args.join(" ")) {
+ id = event.threadID;
+ } else {
+ id = parseInt(args.join(" "));
+ }
+ return api.sendMessage("🚴🏼‍♂️ 𝗯𝗼𝗻𝗻𝗲 𝘀𝘂𝗶𝘁𝗲 ", id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
+		}
+	}
